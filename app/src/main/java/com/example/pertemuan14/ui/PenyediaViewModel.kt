e9c0b08539de1d752cb9ed2056237b0839c488d9
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-01-12 18:49:10 +0700

    Menambahkan inisialisasi untuk InsertViewModel dengan menyertakan dependensi repositoryMhs yang diperoleh dari container di dalam mahasiswaApp()

--- app/src/main/java/com/example/pertemuan14/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/ui/PenyediaViewModel.kt
@@ -1,17 +1,23 @@
 package com.example.pertemuan14.ui
 
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pertemuan14.MahasiswaApplications
+import com.example.pertemuan14.insert.viewmodel.InsertViewModel
 import com.example.pertemuan14.ui.home.viewmodel.HomeViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             HomeViewModel(
+                mahasiswaApp().container.repositoryMhs
+            )
+        }
+
+        initializer {
+            InsertViewModel(
                 mahasiswaApp().container.repositoryMhs
             )
         }
