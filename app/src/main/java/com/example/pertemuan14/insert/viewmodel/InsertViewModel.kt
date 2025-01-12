3fc4f3732d7bd6a637107defe2c0d7bbf5fc998e
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-01-12 18:47:41 +0700

    menambahkan data class berisi nim nama jeniskelamin alamat dan kelas

--- app/src/main/java/com/example/pertemuan14/insert/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/insert/viewmodel/InsertViewModel.kt
@@ -72,4 +72,44 @@ sealed class FormState {
     data class Error(val message: String) : FormState()
 }
 
+data class InsertUiState(
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState()
+)
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
+
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null,
+){
+    fun isValid() : Boolean {
+        return nim == null
+                && nama == null
+                && jenisKelamin == null
+                && alamat == null
+                && kelas == null
+                && angkatan == null
+    }
+}
 
